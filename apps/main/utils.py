79bda81dc22090fdcffef4283a5e8b878ff25002
cbekoder <cbekoder@gmail.com>
2025-01-29 23:42:23 +0500

    update for host

--- apps/main/utils.py
+++ apps/main/utils.py
@@ -1,7 +1,4 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from .models import Income, Expense, MoneyCirculation
-from .serializers import MixedDataSerializer
 
 
 
