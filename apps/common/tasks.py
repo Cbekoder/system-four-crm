28be5c655e5124c44223d150c7e39f7e5e7448c5
cbekoder <cbekoder@gmail.com>
2025-02-25 17:14:39 +0500

    testing celery

--- apps/common/tasks.py
+++ apps/common/tasks.py
@@ -1,8 +1,11 @@
 from django.core.cache import cache
 import requests
+from celery import shared_task
+from .services.logging import Telegram
 # from .utils import redis
 
 
+@shared_task
 def fetch_currency_rate():
     response = requests.get("https://api.exchangerate-api.com/v4/latest/USD")
 
@@ -13,5 +16,6 @@ def fetch_currency_rate():
         if UZS_rate and RUB_rate:
             cache.set("UZS_rate", UZS_rate)
             cache.set("RUB_rate", RUB_rate)
+            Telegram.send_log(f"Rate updated: UZS: {UZS_rate}, RUB: {RUB_rate}")
             return f"Rate cached: UZS: {UZS_rate}, RUB: {RUB_rate}"
     return "Failed to update rate"
--- core/celery.py
+++ core/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery.schedules import crontab
+from apps.common.services.logging import Telegram
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
@@ -12,8 +13,8 @@ app.conf.broker_connection_retry_on_startup = True
 app.autodiscover_tasks()
 
 @app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
+def debug_task():
+    Telegram.send_log("Debug task")
 
 app.conf.beat_schedule = {
     'update_currency_every_midnight': {
