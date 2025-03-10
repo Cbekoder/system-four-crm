dc37e638c0e4d21e59e6b30f333d47125fd944bb
cbekoder <cbekoder@gmail.com>
2025-03-10 06:45:57 +0500

    fix in currency updater

--- apps/common/utils.py
+++ apps/common/utils.py
@@ -31,13 +31,5 @@ def fetch_currency_rate():
             return f"Rate cached: UZS: {UZS_rate}, RUB: {RUB_rate}"
     return Telegram.send_log("Valyutalar kursini olishda muammo")
 
-def convert_currency(base, target, amount):
-    valid_currencies = exchange_rates.keys()
-    if base not in valid_currencies or target not in valid_currencies:
-        raise ValueError("Invalid currency type")
-    base_to_target_rate = exchange_rates[target] / exchange_rates[base]
-    converted_amount = amount * base_to_target_rate
-    return round(converted_amount, 2)
-
 
 
