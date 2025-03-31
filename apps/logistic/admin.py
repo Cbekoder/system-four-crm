412894dd7384dd881b98a97f32ee82575861e8c7
cbekoder <cbekoder@gmail.com>
2025-04-01 02:30:05 +0500

    fix in logistics admin

--- apps/logistic/admin.py
+++ apps/logistic/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import (
-    Driver, Tenant, Contractor, Car, Trailer, CarExpense, LogisticSalaryPayment, TIRRecord,
+    Driver, Tenant, Contractor, Car, Trailer, CarExpense, LogisticSalaryPayment, TIRRecord, ContractRecord,
     # Contract, Transit, TransitExpense, TransitIncome,
 )
 
@@ -49,35 +49,10 @@ class LogisticSalaryPaymentAdmin(admin.ModelAdmin):
     search_fields = ("driver__first_name", "driver__last_name")
     list_filter = ("currency_type", "created_at")
 
-#
-# @admin.register(Contract)
-# class ContractAdmin(admin.ModelAdmin):
-#     list_display = ("number", "contractor")
-#     search_fields = ("number", "contractor__first_name", "contractor__last_name")
-#
-#
-# @admin.register(Transit)
-# class TransitAdmin(admin.ModelAdmin):
-#     list_display = ("car", "driver", "leaving_contract", "leaving_date", "arrival_contract", "arrival_date", "status")
-#     search_fields = ("car__state_number", "driver__first_name", "driver__last_name")
-#     list_filter = ("status", "leaving_date", "arrival_date")
-#
-#
-# @admin.register(TransitExpense)
-# class TransitExpenseAdmin(admin.ModelAdmin):
-#     list_display = ("transit", "reason", "amount", "currency_type", "created_at")
-#     search_fields = ("reason", "transit__id")
-#     list_filter = ("currency_type", "created_at")
-#
-#
-# @admin.register(TransitIncome)
-# class TransitIncomeAdmin(admin.ModelAdmin):
-#     list_display = ("transit", "reason", "amount", "currency_type", "created_at")
-#     search_fields = ("reason", "transit__id")
-#     list_filter = ("currency_type", "created_at")
-#
-#
-# @admin.register(TIRRecord)
-# class TirRecordAdmin(admin.ModelAdmin):
-#     list_display = ("tir", "driver", "car", "trailer", "created_at")
-#     search_fields = ("tir", "driver__first_name", "driver__last_name", "car", "trailer")
+
+@admin.register(ContractRecord)
+class ContractRecordAdmin(admin.ModelAdmin):
+    list_display = ('contract_number', 'date', 'invoice_number', 'contractor', 'amount', 'currency_type', 'remaining', 'status')
+    list_filter = ('status', 'currency_type', 'date')
+    search_fields = ('contract_number', 'invoice_number', 'contractor__name')
+    ordering = ('-date',)
